deb1e766098dfc15262e4c07ced84d4093b64a51
Dave Song <49602639+woony91@users.noreply.github.com>
2020-02-27 11:24:20 -0500

    finalized the fading app

--- app/src/main/java/com/example/animations/MainActivity.java
+++ app/src/main/java/com/example/animations/MainActivity.java
@@ -5,12 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean bartIsShowing = true;
+
     public void fade(View view){
         Log.i("Info", "ImageView has been tapped");
+
+        ImageView bart = (ImageView) findViewById(R.id.iv_bart);
+
+        ImageView homer = (ImageView) findViewById(R.id.iv_homer);
+
+        if (bartIsShowing){
+            bartIsShowing = false;
+            bart.animate().alpha(0).setDuration(2000);
+            homer.animate().alpha(1).setDuration(2000);
+        } else {
+            bartIsShowing = true;
+            bart.animate().alpha(1).setDuration(2000);
+            homer.animate().alpha(0).setDuration(2000);
+        }
+
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
